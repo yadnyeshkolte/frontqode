39f667a9d49cefc57499bffafe634296ce200632
yadnyeshkolte <yadnyeshkolte@gmail.com>
2025-04-18 22:02:06 +0530

    update on the webpack.main.config.ts

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,8 +1,7 @@
 import type { Configuration } from 'webpack';
 
 import { rules } from './webpack.rules';
-import {plugins} from "./webpack.plugins";
-
+import { plugins } from './webpack.plugins';
 
 export const mainConfig: Configuration = {
   /**
